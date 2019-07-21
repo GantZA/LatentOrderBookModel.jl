0684ee23ed51d249dd3cac336d9f22252f180894
Michael Gant <michael.j.g.za@gmail.com>
2019-07-21 19:50:22 +0200

    added parallel functionality

--- src/objective_surface.jl
+++ src/objective_surface.jl
@@ -1,13 +1,26 @@
-function (os::ObjectiveSurface)(seed)
+using SharedArray
+
+function (os::ObjectiveSurface)(seed, para=false)
     iterations = os.surface_points^2 * os.replications
 
-    seeds = Int.(rand(MersenneTwister(seed), UInt32, os.replications, iterations))
-    price_paths = Array{Float64,2}(undef, os.params["T"], iterations)
-    for i in 1:iterations
-        os.params[os.param1_name] = os.param1_values[i]
-        os.params[os.param2_name] = os.param2_values[i]
-        rdpp = ReactionDiffusionPricePath(os.params)
-        price_paths[:,i] = rdpp(seeds[i])
+    seeds = Int.(rand(MersenneTwister(seed), UInt32, iterations))
+    if para==true
+        price_paths = SharedArray{Float32,2}((os.params["T"], iterations))
+        @sync @distributed for i in 1:iterations
+            os.params[os.param1_name] = os.param1_values[i]
+            os.params[os.param2_name] = os.param2_values[i]
+            rdpp = ReactionDiffusionPricePath(os.params)
+            price_paths[:,i] = rdpp(seeds[i])
+        end
+        return price_paths
+    else
+        price_paths = Array{Float32,2}(undef, os.params["T"], iterations)
+        for i in 1:iterations
+            os.params[os.param1_name] = os.param1_values[i]
+            os.params[os.param2_name] = os.param2_values[i]
+            rdpp = ReactionDiffusionPricePath(os.params)
+            price_paths[:,i] = rdpp(seeds[i])
+        end
+        return price_paths
     end
-    return price_paths
 end
