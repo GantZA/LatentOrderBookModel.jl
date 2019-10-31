31dbfc89365d16292520ff5fb388d54576364eef
Michael Gant <michael.j.g.za@gmail.com>
2019-10-31 22:24:46 +0200

    cleaning up and removing commented code

--- src/reaction_diffusion_spde.jl
+++ src/reaction_diffusion_spde.jl
@@ -32,51 +32,34 @@ function dtrw_solver(rdpp::ReactionDiffusionPricePaths)
 
     p =  ones(Float64, rdpp.T) * rdpp.p₀
     ϵ = rand(Normal(0.0,1.0), rdpp.T-1)
-    P⁺s = ones(Float64, rdpp.M+1, rdpp.T-1)
-    # Ps = ones(Float64, rdpp.M+1, rdpp.T-1)
-    P⁻s = ones(Float64, rdpp.M+1, rdpp.T-1)
-    # Simulate SPDE
+    P⁺s = ones(Float64, rdpp.T-1)
+    P⁻s = ones(Float64, rdpp.T-1)
+
     @inbounds for n = 1:rdpp.T-1
         Vₜ =  sign(ϵ[n]) * min(abs(rdpp.σ*ϵ[n]), Δx/Δt)
         V = (-Vₜ .* rdpp.x) ./ (2.0*rdpp.D)
 
-        # P⁺ = vcat(exp.(-rdpp.β.*V[2:end]), exp(-rdpp.β*V[end]))
         P⁺ = 1/(1 + exp(-rdpp.β * Vₜ * Δx / rdpp.D))
-        # P = exp.(-rdpp.β.*V[2:end-1])
         P⁻ = 1 - P⁺
-        # Z = P⁺ .+ P⁻
-        # # Normalizing the probabilities
-        # P⁺ = P⁺ ./ Z
-        # # P = P ./ Z
-        # P⁻ = P⁻ ./ Z
 
-        P⁺s[:,n] .= P⁺
-        # Ps[:,n-1] = P
-        P⁻s[:,n] .= P⁻
+        P⁺s[n] .= P⁺
+        P⁻s[n] .= P⁻
 
         φ₋₁ = φ[1,n] * (1-(Vₜ*Δx)/rdpp.D)
         φₘ₊₁ = φ[end,n] * (1+(Vₜ*Δx)/rdpp.D)
 
-        # φ₋₁ = φ[1,n] * P⁻[2]/P⁺[1]
-        # φₘ₊₁ = φ[end,n] * P⁺[end-1]/P⁻[end]
-
-
         φ[1,n+1] = P⁺ * φ₋₁ +
             P⁻ * φ[2,n] +
-            exp(rdpp.nu*n*Δt) * rdpp.source_term(rdpp.x[1], p[n])
+            rdpp.source_term(rdpp.x[1], p[n])
 
         φ[end,n+1] = P⁻ * φₘ₊₁ +
             P⁺ * φ[end-1,n] +
-            exp(rdpp.nu*n*Δt) * rdpp.source_term(rdpp.x[end], p[n])
+            rdpp.source_term(rdpp.x[end], p[n])
 
-
-        # Compute Interior Points
-        φ[2:end-1,n+1] = P⁺ .* φ[1:end-2,n] +
+        φ[2:end-1,n+1] = P⁺ * φ[1:end-2,n] +
             P⁻ * φ[3:end,n] +
-            [exp(rdpp.nu*n*Δt) * rdpp.source_term(xᵢ, p[n])
-            for xᵢ in rdpp.x[2:end-1]]
+            [rdpp.source_term(xᵢ, p[n]) for xᵢ in rdpp.x[2:end-1]]
 
-        φ[:,n+1] = exp(-rdpp.nu*n*Δt) .* φ[:,n+1]
         p[n+1] = extract_mid_price(rdpp, φ[:,n+1])
     end
 
