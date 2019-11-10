3e1f7226696858a4f3b749ba3d67b1d97c149e80
Michael Gant <michael.j.g.za@gmail.com>
2019-11-10 20:42:57 +0200

    fixed variable name

--- src/objective_surface.jl
+++ src/objective_surface.jl
@@ -14,7 +14,7 @@ function (os::ObjectiveSurface)(seed, para=false)
 
     seeds = Int.(rand(MersenneTwister(seed), UInt32, iterations))
     if para==true
-        price_paths = SharedArray{Float64,2}((os.params["T"], iterations))
+        sample_price_paths = SharedArray{Float64,2}((os.params["T"], iterations))
         @sync @distributed for i in 1:iterations
             os.params[os.param1_name] = os.param1_values[i]
             os.params[os.param2_name] = os.param2_values[i]
@@ -23,9 +23,9 @@ function (os::ObjectiveSurface)(seed, para=false)
             index_end = i * os.replications
             _, _, sample_price_paths[:, index_start:index_end], _, _ = rdpp(seeds[i])
         end
-        return price_paths
+        return sample_price_paths
     else
-        price_paths = Array{Float64,2}(undef, os.params["T"], iterations)
+        sample_price_paths = Array{Float64,2}(undef, os.params["T"], iterations)
         for i in 1:iterations
             os.params[os.param1_name] = os.param1_values[i]
             os.params[os.param2_name] = os.param2_values[i]
