4a005f76f580930d72a6a5a1b8acecfe3126e229
Michael <michael.j.g.za@gmail.com>
2019-12-01 10:15:13 +0200

    removing legacy working code

--- src/reaction_diffusion_spde.jl
+++ src/reaction_diffusion_spde.jl
@@ -131,46 +131,12 @@ function dtrw_solver(rdpp::ReactionDiffusionPricePaths)
         for τₖ = 1:τ_periods
             φ[:, t+1], P⁺s[t], P⁻s[t], Ps[t]  = intra_time_period_simulate(rdpp, φ[:, t], p[calendar_time_index])
             p[t+1] = extract_mid_price(rdpp, φ[:, t+1])
-            if t*rdpp.Δt > calendar_time_index
+            if t*rdpp.Δt >= calendar_time_index
                 calendar_time_index += 1
                 mid_prices[calendar_time_index] = extract_mid_price(rdpp, φ[:, t+1])
             end
             t += 1
         end
-    end
-
-    for t = 1:
-        τ = floor(Int, (1 + mod(n-1, rdpp.Δt))/rdpp.Δt)
-        φ[:, initial_lob_index] = initial_conditions_numerical(rdpp, mid_prices[n])
-
-        for t = 1:τ
-            running_index = initial_lob_index + t
-            ϵ = rand(Normal(0.0, 1.0))
-            Vₜ = sign(ϵ) * min(abs(rdpp.σ * ϵ), rdpp.Δx / rdpp.Δt)
-
-            P⁺, P⁻, P = calculate_jump_probabilities(rdpp, Vₜ)
-
-            P⁺s[running_index-1] = P⁺
-            P⁻s[running_index-1] = P⁻
-            P[running_index-1] = P
-
-            φ₋₁ = φ[1, running_index-1]
-            φₘ₊₁ = φ[end, running_index-1]
-
-            φ[1, running_index] = P⁺ * φ₋₁ + P⁻ * φ[2, running_index-1] +
-                rdpp.source_term(rdpp.x[1], p[running_index-1])
-
-            φ[end, running_index] = P⁻ * φₘ₊₁ + P⁺ * φ[end-1, running_index-1] +
-                          rdpp.source_term(rdpp.x[end], p[running_index-1])
-
-            φ[2:end-1, running_index] = P⁺ * φ[1:end-2, running_index-1] +
-                P⁻ * φ[3:end, running_index-1] +
-                [rdpp.source_term(xᵢ, p[running_index-1]) for xᵢ in rdpp.x[2:end-1]]
-
-            p[running_index] = extract_mid_price(rdpp, φ[:, running_index])
-        end
-        initial_lob_index += τ + 1
-        mid_prices[n+1] = p[initial_lob_index-1]
     end
     return φ, p, mid_prices, P⁺s, P⁻s
 end
