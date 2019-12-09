b8be3ef28cf6d3f16bba846785a0c95065f4784d
Michael <michael.j.g.za@gmail.com>
2019-12-09 22:02:27 +0200

    updated objective surface code to work with latest version of LOB

--- src/objective_surface.jl
+++ src/objective_surface.jl
@@ -1,11 +1,18 @@
+# LOB
 mutable struct ObjectiveSurface
     param1_name::String
-    param1_values::Array{Any,1}
+    param1_values::Array{Any, 1}
     param2_name::String
-    param2_values::Array{Any,1}
+    param2_values::Array{Any, 1}
     surface_points::Int64
     replications::Int64
-    params::Dict{String, Any}
+    parameters::Dict{String, Any}
+end
+function ObjectiveSurface(param1_name::String, param1_values::Array{Float64, 1},
+    param2_name::String, param2_values::Array{Float64, 1},
+    surface_points::Int64, parameters::Dict)
+    return ObjectiveSurface(param1_name, param1_values, param2_name,
+    param2_values, surface_points, parameters["num_paths"], parameters)
 end
 
 
@@ -14,22 +21,22 @@ function (os::ObjectiveSurface)(seed, para=false)
 
     seeds = Int.(rand(MersenneTwister(seed), UInt32, iterations))
     if para==true
-        sample_price_paths = SharedArray{Float64,2}((os.params["T"]+1, iterations*os.replications))
+        sample_price_paths = SharedArray{Float64,2}((os.parameters["T"]+1, iterations*os.replications))
         @sync @distributed for i in 1:iterations
-            os.params[os.param1_name] = os.param1_values[i]
-            os.params[os.param2_name] = os.param2_values[i]
-            rdpp = ReactionDiffusionPricePaths(os.params)
+            os.parameters[os.param1_name] = os.param1_values[i]
+            os.parameters[os.param2_name] = os.param2_values[i]
+            rdpp = ReactionDiffusionPricePaths(os.parameters)
             index_start = (i-1) * os.replications + 1
             index_end = i * os.replications
             _, _, sample_price_paths[:, index_start:index_end], _, _ = rdpp(seeds[i])
         end
         return sample_price_paths
     else
-        sample_price_paths = Array{Float64,2}(undef, os.params["T"]+1, iterations*os.replications)
+        sample_price_paths = Array{Float64,2}(undef, os.parameters["T"]+1, iterations*os.replications)
         for i in 1:iterations
-            os.params[os.param1_name] = os.param1_values[i]
-            os.params[os.param2_name] = os.param2_values[i]
-            rdpp = ReactionDiffusionPricePaths(os.params)
+            os.parameters[os.param1_name] = os.param1_values[i]
+            os.parameters[os.param2_name] = os.param2_values[i]
+            rdpp = ReactionDiffusionPricePaths(os.parameters)
             index_start = (i-1) * os.replications + 1
             index_end = i * os.replications
             _, _, sample_price_paths[:, index_start:index_end], _, _ = rdpp(seeds[i])
