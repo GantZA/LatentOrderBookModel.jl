7728c533f3985b03b57f33b8fd117b658053da7e
Michael <michael.j.g.za@gmail.com>
2019-12-13 17:38:38 +0200

    fixed number of paths to 1 and fixed the mid price paths are printed

--- src/lobm_exec.jl
+++ src/lobm_exec.jl
@@ -5,7 +5,7 @@ using LatentOrderBookModel
 Base.@ccallable function julia_main(ARGS::Vector{String})::Cint
     parsed_args = parse_commandline()
     rdpp = ReactionDiffusionPricePaths(
-        parsed_args["num_paths"],
+        1,
         parsed_args["T"],
         parsed_args["p₀"],
         parsed_args["M"],
@@ -22,7 +22,7 @@ Base.@ccallable function julia_main(ARGS::Vector{String})::Cint
     mid_price_bars,
     P⁺s,
     P⁻s = rdpp(parsed_args["SEED"])
-    print(price_paths[:, 1])
+    print(mid_price_bars[:, 1])
     return 0
 end
 
